4afc8b8872f9b3a039d91b514ce357c0fdc00537
David England <david.england.de@googlemail.com>
2023-11-27 16:39:37 +0000

    Add unlabelled data

--- spawn_sample_data.py
+++ spawn_sample_data.py
@@ -6,6 +6,7 @@ rng = np.random.default_rng(seed=4)
 
 x1 = [4., 12.] + [1., 1.5] * np.random.randn(4096, 2)
 x2 = [3., 9.] + [2.5, .8] * np.random.randn(2048, 2)
+unlabelled = [4., 4.] + [1.5, 1.] * np.random.randn(8192, 2)
 
 x1 = np.concatenate((x1, np.zeros((4096, 1)) + 1.), axis=1)
 x2 = np.concatenate((x2, np.zeros((2048, 1)) + 2.), axis=1)
