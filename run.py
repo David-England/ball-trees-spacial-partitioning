fde1640e6614deaaba88a957123bc4781a525eea
David England <david.england.de@googlemail.com>
2023-11-30 19:38:26 +0000

    Compare approach runtimes

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import datetime
 import ball_trees.spawn_sample_data as ssd
 from ball_trees.constructs import Ball
 import ball_trees.knn as knn
@@ -5,15 +6,25 @@ import ball_trees.knn as knn
 b = Ball(0, ssd.labelled_data[:, :-1], ssd.labelled_data[:, -1], 20)
 
 #---- Naive approach
+dt_nv1 = datetime.datetime.now()
+
 results_nv = []
 for i in range(ssd.unlabelled.shape[0]):
     results_nv.append(knn.knn_naive(3)(ssd.unlabelled[i, :], ssd.labelled_data[:, :-1], ssd.labelled_data[:, -1]))
 
+dt_nv2 = datetime.datetime.now()
+
 #---- Ball tree approach
+dt_bt1 = datetime.datetime.now()
+
 results_bt = []
 for i in range(ssd.unlabelled.shape[0]):
     results_bt.append(knn.knn_ball_tree(3)(ssd.unlabelled[i, :], b))
 
+dt_bt2 = datetime.datetime.now()
+
 #---- Print results to console
+print("NAIVE:", (dt_nv2 - dt_nv1).seconds, "s")
 print(results_nv[:15])
+print("BALL TREE:", (dt_bt2 - dt_bt1).seconds, "s")
 print(results_bt[:15])
